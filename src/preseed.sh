2dc0bc71a6df107118f7385cd0cae5af3885c65c
Dmitry Sharshakov <dmitry.sharshakov@siderolabs.com>
2025-01-30 17:46:18 +0100

    chore: add GitHub Actions

--- src/preseed.sh
+++ src/preseed.sh
@@ -8,10 +8,9 @@ fetch(){
 	local package=$(echo ${line} | sed 's/^[a-z0-9]\+ \(.*\)/\1/g');
 	local hash=$(echo ${line} | sed 's/^\([a-z0-9]\+\) .*/\1/g');
 	local ref=$(printf "stagex/%s@sha256:%s" "$package" "$hash");
-	docker pull ${ref}
 	rm -rf "out/${package}"
 	mkdir -p "out/${package}"
-	docker save ${ref} | tar -xC out/${package}
+	crane pull --format oci ${ref} out/${package}
 }
 
 while read line; do
